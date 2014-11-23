6e18bc11a831b401a3c2d8ace16ed67761ef9cd6
Andrew Chappell <acha5066@gmail.com>
2014-11-24 00:05:45 +1100

    Updated manifest files. Organized fonts

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -2,13 +2,20 @@
 description "SS-Framework is a front-end framework for building websites more quickly."
 
 # Stylesheet Import
-file '_base.scss', :like => :stylesheet, :media => 'screen, projection'
-file 'gluhu.scss', :like => :stylesheet, :media => 'screen, projection'
+stylesheet '_base.scss', :media => 'screen, projection'
+stylesheet 'gluhu.scss', :media => 'screen, projection'
 
-discover :all
 
 # Javascript Import
 
+javascript 'toggle.js', :within => 'javascripts_dir'
+
+# Font import 
+
+font 'icomoon.eot', :within => 'fonts_dir'
+font 'icomoon.svg', :within => 'fonts_dir'
+font 'icomoon.ttf', :within => 'fonts_dir'
+font 'icomoon.woff', :within => 'fonts_dir'
 
 # file 'scripts.js', :like => :javascript, :to => 'scripts.js'
 
@@ -17,11 +24,12 @@ discover :all
 
 # Compass Extension Help
 help %Q{
-  Help is available on the Github repo. 
+  Help is available on the Github repo. https://github.com/acha5066/gluhu
 }
 
 # Compass Extension Welcome Message
 #  Users will see this when they create a new project using this template.
 welcome_message %Q{
-  Welcome to Gluhu, a front end framework ready to drop in to your compass project. May the code be ever in your favour.
+  Welcome to Gluhu, a compass extension to speed up front end development. 
+  May the code be ever in your favour.
 }
